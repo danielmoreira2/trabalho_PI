7085bc33f7fa35b67db1ae6bf1a9685c30482d9d
daniel <daniel.moreira@edu.unifil.br>
2020-03-28 13:12:18 -0300

    trabalho

---
+++ index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function Home(){
+    return (
+       <div>
+           <Head>
+              <title>Meu titulo</title>
+           </Head>
+         <h1>Quem Somos</h1>       
+
+       </div>
+
+ );
+
+};
+
+export default Home;
